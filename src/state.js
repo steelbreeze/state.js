2f4fccb750dadc39d687cc17921cc5de027e8a41
mesmo <mesmo@steelbreeze.net>
2013-05-14 13:55:01 +0100

    More work...
    Created LINQ-like methods for Array.
    Fixed bug in state completion transitions.
    Added multiple initial state check.
    Linted code.

--- src/state.js
+++ src/state.js
@@ -1,6 +1,33 @@
+Array.prototype.single = function( predicate )
+{
+	var results = predicate ? this.filter( predicate ) : this;
+	
+	if( results.length != 1 )
+	{
+		throw new Error( "Cannot return more than one item" );
+	}
+	
+	return results[ 0 ];
+};
+
+Array.prototype.singleOrDefault = function( predicate )
+{
+	var results = predicate ? this.filter( predicate ) : this;
+	
+	if( results.length > 1 )
+	{
+		throw new Error( "Cannot return more than one item" );
+	}
+	
+	if( results.length == 1 )
+	{
+		return results[ 0 ];
+	}
+};
+
 PseudoStateKind = // TODO: other kinds
 {
-	Initial : { Name: "initial", IsInitial: true, IsHistory: false, GetCompletion: function( completions ) { return completions[ 0 ]; } }
+	Initial : { Name: "initial", IsInitial: true, IsHistory: false, GetCompletion: function( completions ) { return completions.single(); } }
 };
 
 function Region( name, parent )
@@ -14,7 +41,6 @@ function Region( name, parent )
 
 	this.Name = name;
 	this.Parent = parent;
-	this.Initial = null;
 	this.IsActive = false;
 	this.Vertices = [];
 }
@@ -29,13 +55,15 @@ Region.prototype =
 	Initialise : function( deepHistory )
 	{
 		if( !deepHistory )
+		{
 			deepHistory = false;
+		}
 		
 		this.BeginEnter();
 		
-		vertex = ( ( deepHistory || this.Initial.Kind.IsHistory ) && this.Current ) ? this.Current : this.Initial;
+		var vertex = ( ( deepHistory || this.Initial.Kind.IsHistory ) && this.Current ) ? this.Current : this.Initial;
 
-		vertex.Initialise( deepHistory ); // TODO: add || Inital.Kind == DeepHistory
+		vertex.Initialise( deepHistory ); // TODO: add || Initial.Kind == DeepHistory
 	},
 
 	Process : function( message )
@@ -45,9 +73,11 @@ Region.prototype =
 
 	OnExit : function()
 	{
-		if( this.Current && this.Current.IsActive == true )
+		if( this.Current && this.Current.IsActive === true )
+		{
 			this.Current.OnExit();
-	
+		}
+		
 		console.log( "Leave: " + this.toString() );
 		
 		this.IsActive = false;
@@ -55,9 +85,11 @@ Region.prototype =
 
 	BeginEnter : function()
 	{
-		if( this.IsActive == true )
+		if( this.IsActive === true )
+		{
 			this.OnExit();
-	
+		}
+		
 		console.log( "Enter: " + this );
 			
 		this.IsActive = true;
@@ -67,24 +99,33 @@ Region.prototype =
 	{
 		return this.Parent ? this.Parent.toString() + "." + this.Name : this.Name;
 	}
-}
+};
 
 function PseudoState( kind, parent )
 {
+	// use a state's default region if a state was provided
 	if( parent instanceof State )
+	{
 		parent = parent.DefaultRegion();
+	}
 
-	// TODO: assert if kind is defined in PseudoStateKind
+	// validate parameters
 	console.assert( parent instanceof Region, "parent must be a Region" );
+	// TODO: assert if kind is defined in PseudoStateKind
 
-	parent.Vertices.push( this );
-
-	if( kind.IsInitial == true )
-		parent.Initial = this;
-	
 	this.Kind = kind;
 	this.Parent = parent;
 	this.Completions = [];
+
+	if( kind.IsInitial === true )
+	{		
+		if( parent.Initial )
+			throw new Error( "Regions can have at most 1 initial pseudo state" );
+	
+		parent.Initial = this;
+	}
+	
+	parent.Vertices.push( this );
 }
 
 PseudoState.prototype = 
@@ -116,15 +157,17 @@ PseudoState.prototype =
 	{
 		return this.Parent ? this.Parent.toString() + "." + this.Kind.Name : this.Kind.Name;
 	}
-}
+};
 	
 function State( name, parent )
 {
 	if( parent )
 	{
 		if( parent instanceof State )
+		{
 			parent = parent.DefaultRegion();
-			
+		}
+		
 		console.assert( parent instanceof Region, "parent must be a Region" );
 			
 		parent.Vertices.push( this );
@@ -144,14 +187,16 @@ State.prototype =
 {
 	IsSimple : function()
 	{
-		return this.Regions.length == 0;
+		return this.Regions.length === 0;
 	},
 	
 	DefaultRegion: function()
 	{
 		if( !this.defaultRegion )
+		{
 			this.defaultRegion = new Region( "default", this );
-			
+		}
+		
 		return this.defaultRegion;
 	},
 	
@@ -163,20 +208,28 @@ State.prototype =
 	
 	Process : function( message )
 	{
-		var transition = this.Transitions.filter( function( t ) { return t.Guard( message ); } );
-		var processed = transition.length == 1;
-		
-		if( processed == true )
+		var transition = this.Transitions.singleOrDefault( function( t ) { return t.Guard( message ); } );
+		var processed = transition ? true : false;
+					
+		if( processed === true )
 		{
-			transition[ 0 ].Traverse( message );
+			transition.Traverse( message );
 		}
 		else
 		{
-			if( this.IsSimple() == false )
+			if( this.IsSimple() === false )
+			{
 				for( var i = 0; i < this.Regions.length; i++ )
-					if( this.Regions[ i ].IsActive == true )
-						if( this.Regions[ i ].Process( message ) == true )
+				{
+					if( this.Regions[ i ].IsActive === true )
+					{
+						if( this.Regions[ i ].Process( message ) === true )
+						{
 							processed = true;
+						}
+					}
+				}
+			}
 		}
 		
 		return processed;
@@ -184,7 +237,7 @@ State.prototype =
 
 	OnExit : function()
 	{
-		this.Regions.forEach( function( region ) { if( region.IsActive ) region.OnExit(); } );
+		this.Regions.forEach( function( region ) { if( region.IsActive ) { region.OnExit(); } } );
 			
 		this.Exit.forEach( function( action ) { action(); } );
 
@@ -195,15 +248,19 @@ State.prototype =
 
 	BeginEnter : function()
 	{
-		if( this.IsActive == true )
+		if( this.IsActive === true )
+		{
 			this.OnExit();
-	
+		}
+		
 		console.log( "Enter: " + this.toString() );
 			
 		this.IsActive = true;
 			
 		if( this.Parent )
+		{
 			this.Parent.Current = this;
+		}
 		
 		this.Entry.forEach( function( action ) { action(); } );
 	},
@@ -214,12 +271,14 @@ State.prototype =
 		
 		if( this.Completions.length > 0 )
 		{
-			if( this.IsSimple() || this.Regions.every( function( region ) { region.IsComplete(); } ) )
+			if( this.IsSimple() || this.Regions.every( function( region ) { return region.IsComplete(); } ) )
 			{
-				var completion = this.Completions.filter( function( c ) { c.Guard(); } );
+				var completion = this.Completions.singleOrDefault( function( c ) { return c.Guard(); } );
 				
-				if( completion.length == 1 )
-					completion[ 0 ]( deepHistory );
+				if( completion )
+				{
+					completion( deepHistory );
+				}
 			}
 		}
 	},
@@ -228,20 +287,22 @@ State.prototype =
 	{
 		return this.Parent ? this.Parent.toString() + "." + this.Name : this.Name;
 	}
-}
+};
 
 function FinalState( name, parent )
 {
 	if( parent instanceof State )
+	{
 		parent = parent.DefaultRegion();
-
+	}
+	
 	console.assert( parent instanceof Region, "parent must be a Region" );
-
-	parent.Vertices.push( this );
 
 	this.Name = name;
 	this.Parent = parent;
 	this.IsActive = false;
+
+	parent.Vertices.push( this );
 }
 
 FinalState.prototype = 
@@ -258,15 +319,19 @@ FinalState.prototype =
 
 	BeginEnter : function()
 	{
-		if( this.IsActive == true )
+		if( this.IsActive === true )
+		{
 			this.OnExit();
-	
+		}
+		
 		console.log( "Enter: " + this.toString() );
 			
 		this.IsActive = true;
 			
 		if( this.Parent )
+		{
 			this.Parent.Current = this;
+		}
 	},
 
 	EndEnter : function( deepHistory )
@@ -277,7 +342,7 @@ FinalState.prototype =
 	{
 		return this.Parent ? this.Parent.toString() + "." + this.Name : this.Name;
 	}
-}
+};
 
 function Completion( source, target, guard )
 {
@@ -300,7 +365,7 @@ Completion.prototype =
 		
 		this.Path.Complete( deepHistory );
 	}
-}
+};
 
 function Transition( source, target, guard )
 {
@@ -325,14 +390,18 @@ Transition.prototype =
 		
 		this.Path.Complete( false );
 	}
-}
+};
 
 function Ancestors( node )
 {
 	if( node.Parent )
+	{
 		return Ancestors( node.Parent ).concat( node );
+	}
 	else
+	{
 		return [ node ];
+	}
 }
 
 function Path( source, target )
@@ -340,18 +409,24 @@ function Path( source, target )
 	var path = { Exit: [], Enter: [] };
 	var sourceAncestors = Ancestors( source );
 	var targetAncestors = Ancestors( target );
+	var i = 0;
 	
-	for( var i = 0; sourceAncestors[ i ] == targetAncestors[ i ]; i++ );
+	while( sourceAncestors[ i ] == targetAncestors[ i ] )
+	{
+		i++;
+	}
 	
-	if( source instanceof PseudoState )
-		if( sourceAncestors[ i ] != source )
-			path.Exit.push( function() { source.OnExit(); } );
+	if( source instanceof PseudoState && ( sourceAncestors[ i ] != source ) )
+	{
+		path.Exit.push( function() { source.OnExit(); } );
+	}
 	
-	var sourceAncestor = sourceAncestors[ i ];
-	path.Exit.push( function() { sourceAncestor.OnExit(); } );
+	( function( s ) { path.Exit.push( function() { s.OnExit(); } ); } )( sourceAncestors[ i ] );
 		
-	for( ; i < targetAncestors.length; i++ )
-		( function( t ) { path.Enter.push( function() { t.BeginEnter(); } ); } )( targetAncestors[ i ] );
+	while( i < targetAncestors.length )
+	{
+		( function( t ) { path.Enter.push( function() { t.BeginEnter(); } ); } )( targetAncestors[ i++ ] );
+	}
 			
 	path.Complete = function() { target.EndEnter(); };
 
