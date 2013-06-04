027eb87b1da822d446c34212a6e55fdf6553c608
mesmo <mesmo@steelbreeze.net>
2013-06-04 09:53:53 +0100

    Added every method to oldie.js
    Last refactor used the every method which is not available in older
    versions of IE.

--- src/oldie.js
+++ src/oldie.js
@@ -44,6 +44,31 @@ if (!Array.prototype.filter)
 	};
 }
 
+if (!Array.prototype.every)
+{
+  Array.prototype.every = function(fun /*, thisp */)
+  {
+    "use strict";
+ 
+    if (this == null)
+      throw new TypeError();
+ 
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun != "function")
+      throw new TypeError();
+ 
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
+    {
+      if (i in t && !fun.call(thisp, t[i], i, t))
+        return false;
+    }
+ 
+    return true;
+  };
+}
+
 // Production steps of ECMA-262, Edition 5, 15.4.4.19
 // Reference: http://es5.github.com/#x15.4.4.19
 if (!Array.prototype.map) {
