26b7239bf02ded288406228e8a7691756491c0aa
David Mesquita-Morris <mesmo@steelbreeze.net>
2015-06-10 17:17:33 +0100

    Add a test for callbacks and try istanbul coverage

---
+++ test/callbacks.js
@@ -0,0 +1,42 @@
+/* global describe, it */
+var assert = require("assert"),
+	state = require("../lib/state.com.js");
+
+var instance = new state.StateMachineInstance("test");
+instance.calls = 0;
+instance.logs = 0;
+
+var model = new state.StateMachine("model").setLogger({ log: function (text) { instance.logs++; } });
+var initial = new state.PseudoState("initial", model, state.PseudoStateKind.Initial);
+var stateA = new state.State("stateA", model).exit(function (message, instance) {instance.calls += 1;} );
+var stateB = new state.State("stateB", model).entry(function (message, instance) {instance.calls += 2;});
+
+initial.to(stateA);
+stateA.to(stateB).when(function (message) { return message === "move"; }).effect(function (message, instance) {instance.calls += 4;});
+
+state.initialise(model, instance);
+
+state.evaluate(model, instance, "move");
+
+describe("test/callbacks.js", function () {
+	describe("User defined behavior", function () {
+		it("State exit behavior called", function () {
+			assert.equal(1, 1 & instance.calls);
+		});
+		
+		it("State entry behavior called", function () {
+			assert.equal(2, 2 & instance.calls);
+		});
+		
+		it("State transition behavior called", function () {
+			assert.equal(4, 4 & instance.calls);
+		});
+	});
+	
+	describe("Custom logging", function () {
+		it("Logger called during initialisation and state transitions", function () {
+			assert.equal(10, instance.logs);
+		});
+	});
+});
+
