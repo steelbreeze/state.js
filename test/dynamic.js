804c0131acb8b62f725a5a2016da3999434b0224
David Mesquita-Morris <mesmo@steelbreeze.net>
2015-06-14 11:09:16 +0100

    Add the start of a test for dynamic model building

---
+++ test/dynamic.js
@@ -0,0 +1,31 @@
+/* global describe, it */
+var assert = require("assert"),
+	state = require("../lib/state.com.js");
+
+var model = new state.StateMachine("model").setLogger(console);
+var initial = new state.PseudoState("initial", model, state.PseudoStateKind.Initial);
+var stateA = new state.State("stateA", model).exit(function (message, instance) {instance.calls += 1;} );
+
+initial.to(stateA);
+
+var instance = new state.StateMachineInstance("instance");
+
+state.initialise(model, instance);
+
+//describe("test/callbacks.js", function () {	
+//	describe("With half the model defined:", function () {
+//		it("Model will not respond to events", function () {
+			assert.equal(false, state.evaluate(model, instance, "move"));
+//		});
+//	});
+
+	var stateB = new state.State("stateB", model).entry(function (message, instance) {instance.calls += 2;});
+
+	stateA.to(stateB).when(function (message) { return message === "move"; }).effect(function (message, instance) {instance.calls += 4;});
+	
+//	describe("With the full model defined:", function () {
+//		it("Model will respond to events", function () {
+			assert.equal(true, state.evaluate(model, instance, "move"));
+//		});
+//	});	
+//});
