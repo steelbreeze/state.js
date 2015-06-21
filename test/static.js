462f491456c436ef0cfa2859804d2f5aa273fc37
David Mesquita-Morris <mesmo@steelbreeze.net>
2015-06-21 17:27:53 +0100

    Added test for static conditional branch

---
+++ test/static.js
@@ -0,0 +1,34 @@
+/* global describe, it */
+var assert = require("assert"),
+	state = require("../lib/state.com.js");
+
+var model = new state.StateMachine("model");
+var initial = new state.PseudoState("initial", model, state.PseudoStateKind.Initial);
+var junction1 = new state.PseudoState("junction1", model, state.PseudoStateKind.Junction);
+var junction2 = new state.PseudoState("junction2", model, state.PseudoStateKind.Junction);
+
+var pass = new state.State("success", model);
+var fail = new state.State("error", model);
+
+initial.to(junction1);
+
+junction1.to(junction2).when(function(message, instance) { return instance.counter === 0; }).effect(function(message, instance) { return instance.counter++; });
+junction1.to(fail).else();
+junction2.to(pass).when(function(message, instance) { return instance.counter === 0; }).effect(function(message, instance) { return instance.counter++; });
+junction2.to(fail).else();
+
+var instance = new state.StateMachineInstance();
+instance.counter = 0;
+
+describe("test/static.js", function () {
+	it("Junction transitions implement a static conditional branch", function(){
+		state.initialise(model, instance);
+		
+		assert.equal(pass, instance.getCurrent(model.defaultRegion()));
+	});
+
+	it("Junction transitions call all transition behaviour after guards have been tested", function(){
+		
+		assert.equal(2, instance.counter);
+	});
+});
