93785b65de34ed73ea0c224528ffd09e7369f02f
mesmo <mesmo@steelbreeze.net>
2016-09-15 07:25:41 +0100

    Tidy comments

--- examples/node/JSONInstance.js
+++ examples/node/JSONInstance.js
@@ -1,17 +1,23 @@
 "use strict";
 var state = require("../../lib/state.com");
+/** Manages the active state configuration of a state machine instance using a serializable JSON structure. */
 var JSONInstance = (function () {
+    /** Creates a new instance of the JSONInstance class. */
     function JSONInstance(name) {
         this.name = name;
+        /** Indicates that the state machine instance has reached a [[PseudoStateKind.Terminate]] [[PseudoState]] and therfore will no longer respond to messages. */
         this.isTerminated = false;
     }
+    /** Sets the current State for a given Region. */
     JSONInstance.prototype.setCurrent = function (region, state) {
         this.getNode(region).lastKnown = state.name;
     };
+    /** Returns the current State for a given Region. */
     JSONInstance.prototype.getCurrent = function (region) {
         var lastKnown = this.getNode(region).lastKnown;
         return region.vertices.reduce(function (result, item) { return item instanceof state.State && item.name === lastKnown ? item : result; }, undefined);
     };
+    /** Finds a node within the active state configuration for a given Region. */
     JSONInstance.prototype.getNode = function (state) {
         if (state.parent) {
             var parentNode = this.getNode(state.parent);
@@ -29,6 +35,7 @@ var JSONInstance = (function () {
             return this.activeStateConfiguration;
         }
     };
+    /** Returns the Ac */
     JSONInstance.prototype.toJSON = function () {
         return JSON.stringify(this.activeStateConfiguration);
     };
--- examples/node/JSONInstance.ts
+++ examples/node/JSONInstance.ts
@@ -1,27 +1,41 @@
 import * as state from "../../lib/state.com";
 
+/** Definition of a node within a JSON representation of the active state configuration of a state machine model. */
 interface IJSONNode {
+    /** The name of the named element. */
     name: string;
+
+    /** Child nodes. */
     children: IJSONNode[];
+
+    /** The last known state for nodes representing regions. */
     lastKnown?: string;
 }
 
+/** Manages the active state configuration of a state machine instance using a serializable JSON structure. */
 export class JSONInstance implements state.IInstance {
+    /** The active state configuration represented as a JSON object */
     private activeStateConfiguration: IJSONNode;
+
+    /** Indicates that the state machine instance has reached a [[PseudoStateKind.Terminate]] [[PseudoState]] and therfore will no longer respond to messages. */
     public isTerminated: boolean = false;
 
+    /** Creates a new instance of the JSONInstance class. */
     constructor(private name: string) { }
 
+    /** Sets the current State for a given Region. */
     public setCurrent(region: state.Region, state: state.State): void {
         this.getNode(region).lastKnown = state.name;
     }
 
+    /** Returns the current State for a given Region. */
     public getCurrent(region: state.Region): state.State {
         let lastKnown = this.getNode(region).lastKnown;
 
         return region.vertices.reduce<state.State>((result, item) => item instanceof state.State && item.name === lastKnown ? item : result, undefined);
     }
 
+    /** Finds a node within the active state configuration for a given Region. */
     private getNode(state: state.State | state.Region): IJSONNode {
         if (state.parent) {
             let parentNode = this.getNode(state.parent);
@@ -43,6 +57,7 @@ export class JSONInstance implements state.IInstance {
         }
     }
 
+    /** Returns the Ac */
     public toJSON(): string {
         return JSON.stringify(this.activeStateConfiguration);
     }
