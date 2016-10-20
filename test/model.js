6c3c2ad95fc835c560721154781bea08da7be673
mesmo <mesmo@steelbreeze.net>
2016-10-20 18:41:54 +0100

    Early model building

---
+++ test/model.js
@@ -0,0 +1,13 @@
+var state = require("../lib/node/state");
+
+var model = new state.StateMachine("model");
+var initial = new state.PseudoState("initial", model);
+var stateA = new state.State("stateA", model);
+var stateB = new state.State("stateB", model);
+
+initial.to(stateA);
+stateA.to(stateB).when(function (message) { return message === "move" });
+
+var visitor = new state.Visitor();
+
+model.accept(visitor);
