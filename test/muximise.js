b232c65fd2c5e7afa3b0d176c9421d7ecc6f35f9
mesmo <mesmo@steelbreeze.net>
2016-11-16 19:03:14 +0000

    More tests

--- test/muximise.js
+++ test/muximise.js
@@ -2,7 +2,10 @@
 var assert = require("assert"),
 	state = require("../lib/node/state");
 
-var model = new state.StateMachine( "model" );
+var oldConsole = state.console;
+state.setConsole(console);
+
+var model = new state.StateMachine("model");
 
 var initial = new state.PseudoState("initial", model, state.PseudoStateKind.Initial);
 var ortho = new state.State("ortho", model);
@@ -28,71 +31,55 @@ i2.to(s2);
 
 ortho.to(final); // This should happen once all regions in ortho are complete?
 
-s1.to(f1).when(function(c) {
-    return c === "complete1";
+s1.to(f1).when(function (c) {
+	return c === "complete1";
 });
 
-s2.to(f2).when(function(c) {
-    return c === "complete2";
+s2.to(f2).when(function (c) {
+	return c === "complete2";
 });
 
-ortho.to(simple).when(function(c) {
-    return c === "jump";
+ortho.to(simple).when(function (c) {
+	return c === "jump";
 });
 
-simple.to(ortho).when(function(c) {
-    return c === "back";
+simple.to(ortho).when(function (c) {
+	return c === "back";
 });
 
-state.validate(model);
-
-var instance = new state.StateMachineInstance("instance");
-state.initialise(model, instance);
-
-console.log("simple.isSimple = " + simple.isSimple());
-console.log("simple.isComposite = " + simple.isComposite());
-console.log("simple.isOrthogonal = " + simple.isOrthogonal());
-
-console.log("model.isSimple = " + model.isSimple());
-console.log("model.isComposite = " + model.isComposite());
-console.log("model.isOrthogonal = " + model.isOrthogonal());
-
-console.log("ortho.isSimple = " + ortho.isSimple());
-console.log("ortho.isComposite = " + ortho.isComposite());
-console.log("ortho.isOrthogonal = " + ortho.isOrthogonal());
-
+var instance = new state.DictionaryInstance("instance");
+model.initialise(instance);
 
 describe("test/muximise.js", function () {
 	describe("State type tests", function () {
 		// ensure only simple states return true for isSimple
 		it("simple state isSimple", function () {
 			assert(simple.isSimple());
-			assert(!model.isSimple());
 			assert(!ortho.isSimple());
 		});
 
 		// ensure only composite states return true for isComposite
 		it("State.isComposite", function () {
 			assert(!simple.isComposite());
-			assert(model.isComposite());
 			assert(ortho.isComposite());
 		});
 
 		// ensure only orthogonal states return true for isOrthogonal
 		it("State.isOrthogonal", function () {
 			assert(!simple.isOrthogonal());
-			assert(!model.isOrthogonal());
 			assert(ortho.isOrthogonal());
 		});
 	});
 
 	describe("Orthogonal state completion", function () {
 		// ensure that completion transitions for orthogonal states are triggered after completion of all child regions
-		it("Completion transition fires once all regions of an orthogonal state are complete", function(){
-			state.evaluate(model, instance, "complete1");
-			state.evaluate(model, instance, "complete2");
+		it("Completion transition fires once all regions of an orthogonal state are complete", function () {
+			model.evaluate(instance, "complete1");
+			model.evaluate(instance, "complete2");
 
-			assert.equal(true, state.isComplete(model, instance));
+			assert.equal(true, model.isComplete(instance));
 		});
 	});
 });
+
+state.setConsole(oldConsole);
