68d2f8490dea4fc9c4cec4c29978d52bb6d2b450
mesmo <mesmo@steelbreeze.net>
2017-03-28 19:58:51 +0100

    Complete tree documentation

--- lib/node/tree.d.ts
+++ lib/node/tree.d.ts
@@ -8,7 +8,7 @@ export interface INode {
 /**
  * A [[Node]] within a tree.
  * @param TParent The type of the [[Node]]'s parent [[Node]].
- * @param TChildren The type of the [[Node]]'s children [[Node]]s.
+ * @param TChildren A common type shared by all the [[Node]]'s children.
  */
 export interface Node<TParent extends INode, TChildren extends INode> extends INode {
     /** The parent node of this node. */
@@ -18,8 +18,15 @@ export interface Node<TParent extends INode, TChildren extends INode> extends IN
 }
 /**
  * Returns the ancestry of the [[Node]] from the root [[Node]] to the [[Node]] provided.
- * @param TParent The type of the [[Node]]'s parent [[Node]].
+ * @param TNode A common type shared by all [[Node]] instances within the tree.
  * @param node The [[Node]] to return the ancestry for.
  */
-export declare function Ancestors<TParent extends INode>(node: TParent): Array<TParent>;
-export declare function LowestCommonAncestorIndex<TParent extends INode>(ancestry1: Array<TParent>, ancestry2: Array<TParent>): number;
+export declare function Ancestors<TNode extends INode>(node: TNode): Array<TNode>;
+/**
+ * Returns the index of the lowest/least common ancestor of a pair of nodes within a Tree.
+ * @param TNode A common type shared by all [[Node]] instances within the tree.
+ * @param ancestry1 A [[Node]] within the tree.
+ * @param ancestry2 A [[Node]] within the tree.
+ * @returns The index of the lowest/least common ancestor or -1 if the nodes do not share any ancestry.
+ */
+export declare function LowestCommonAncestorIndex<TNode extends INode>(ancestry1: Array<TNode>, ancestry2: Array<TNode>): number;
--- src/tree.ts
+++ src/tree.ts
@@ -10,7 +10,7 @@ export interface INode {
 /**
  * A [[Node]] within a tree.
  * @param TParent The type of the [[Node]]'s parent [[Node]].
- * @param TChildren The type of the [[Node]]'s children [[Node]]s.
+ * @param TChildren A common type shared by all the [[Node]]'s children.
  */
 export interface Node<TParent extends INode, TChildren extends INode> extends INode {
 	/** The parent node of this node. */
@@ -22,18 +22,25 @@ export interface Node<TParent extends INode, TChildren extends INode> extends IN
 
 /**
  * Returns the ancestry of the [[Node]] from the root [[Node]] to the [[Node]] provided.
- * @param TParent The type of the [[Node]]'s parent [[Node]].
+ * @param TNode A common type shared by all [[Node]] instances within the tree.
  * @param node The [[Node]] to return the ancestry for.
  */
-export function Ancestors<TParent extends INode>(node: TParent): Array<TParent> {
-	const result = node.parent ? Ancestors(node.parent) : new Array<TParent>();
+export function Ancestors<TNode extends INode>(node: TNode): Array<TNode> {
+	const result = node.parent ? Ancestors(node.parent) : new Array<TNode>();
 
 	result.push(node);
 
 	return result;
 }
 
-export function LowestCommonAncestorIndex<TParent extends INode>(ancestry1: Array<TParent>, ancestry2: Array<TParent>): number {
+/**
+ * Returns the index of the lowest/least common ancestor of a pair of nodes within a Tree.
+ * @param TNode A common type shared by all [[Node]] instances within the tree.
+ * @param ancestry1 A [[Node]] within the tree.
+ * @param ancestry2 A [[Node]] within the tree.
+ * @returns The index of the lowest/least common ancestor or -1 if the nodes do not share any ancestry.
+ */
+export function LowestCommonAncestorIndex<TNode extends INode>(ancestry1: Array<TNode>, ancestry2: Array<TNode>): number {
 	let result = 0;
 
 	while (result < ancestry1.length && result < ancestry2.length && ancestry1[result] === ancestry2[result]) {
