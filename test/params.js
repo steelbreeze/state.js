4156e9e52691630774c92ee606db0ba12cb36af8
mesmo <mesmo@steelbreeze.net>
2017-04-06 19:36:16 +0100

    Comment out params test

--- test/params.js
+++ test/params.js
@@ -3,14 +3,14 @@ var assert = require("assert"),
 	state = require("../lib/node/state");
 
 var oldLogger = state.setLogger(console);
-
+/*
 var model = new state.StateMachine("model");
 var initial = new state.PseudoState("initial", model);
 var first = new state.State("first", model).exit(function (i, ...p) { i.exitParams = p; });
 var second = new state.State("second", model).entry(function (i, ...p) { i.entryParams = p; });
 
 initial.to(first);
-/*
+
 first.to(second).when(function (i, ...p) { return p[1] === "closer"; }).effect(function (i, ...p) { i.transParams = p; });
 
 var instance = new state.DictionaryInstance("instance");
