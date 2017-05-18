188e57088aa70250ce235cd1be973179a2ea04bc
mesmo <mesmo@steelbreeze.net>
2017-05-18 19:16:13 +0100

    Daily 3

--- src/state.ts
+++ src/state.ts
@@ -596,7 +596,7 @@ export class Transition {
 	 * @param kind The kind of the [transition]{@link Transition}; use this to explicitly set [local transition]{@link TransitionKind.Local} semantics as needed.
 	 */
 	public constructor(public readonly source: Vertex, public readonly target?: Vertex, public readonly kind: TransitionKind = TransitionKind.External) {
-		this.guard = source instanceof PseudoState ? ():boolean => true : (instance: IInstance, ...message: any[]): boolean => message[0] === this.source;
+		this.guard = source instanceof PseudoState ? (): boolean => true : (instance: IInstance, ...message: any[]): boolean => message[0] === this.source;
 		this.source.outgoing.push(this);
 
 		// validate and repair if necessary the user supplied transition kind
