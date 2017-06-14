c03c372aa9e52d25ba1b613ad9a87749e0682d83
mesmo <mesmo@steelbreeze.net>
2017-06-14 19:03:49 +0100

    Last tidy of delegate.ts

--- lib/node/delegate.d.ts
+++ lib/node/delegate.d.ts
@@ -1,31 +1,23 @@
 /**
- * @module delegate
- *
- * multicast delegate for TypeScript
- *
- * @copyright (c) 2017 David Mesquita-Morris
- *
- * Licensed under the MIT and GPL v3 licences
- */
-/**
  * Implementation of a multicast delegate to be used in conjunction with the [[create]] function.
  */
 export interface Delegate {
     /**
      * The a prototype for any callable function.
      * @param args An arbitory number of parameters to pass to the function.
-     * @return An optional return from the function.
+     * @return An optional return from the function. If the delegate is a multicast delegate (one created from multiple other delegates) this will be in the form of an array.
      */
     (...args: any[]): any;
 }
-/***
- * A delegate that does nothing; this is always returned from create if no delegates are passed in. This can be used for comparison purposes.
- * @param args An arbitory number of parameters to pass to the function.
- */
-export declare const noop: Delegate;
 /**
- * Creates a delegate for one or more functions that can be called as one.
- * @param delegates The set of functions to aggregate into a single delegate.
- * @return Returns a delegate that when called calls the other functions provided.
+ * Tests a [delegate]{@link Delegate} to see if is contains callable behavior.
+ * @param delegate The [delegate]{@link Delegate} to test.
+ * @return Returns true if the [delegate]{@link Delegate} contains callable behavior.
+ */
+export declare function isCallable(delegate: Delegate): boolean;
+/**
+ * Creates a [delegate]{@link Delegate} for one or more [delegates]{@link Delegate} (functions) that can be called as one.
+ * @param delegates The set of [delegates]{@link Delegate} (functions) to aggregate into a single [delegate]{@link Delegate}.
+ * @return Returns a [delegate]{@link Delegate} that when called calls the other [delegates]{@link Delegate} provided.
  */
 export declare function create(...delegates: Delegate[]): Delegate;
--- src/delegate.ts
+++ src/delegate.ts
@@ -1,12 +1,18 @@
 /**
  * @module delegate
  * 
- * multicast delegate for TypeScript
+ * Multicast delegate for TypeScript
  * 
  * @copyright (c) 2017 David Mesquita-Morris
  * 
  * Licensed under the MIT and GPL v3 licences
  */
+
+/***
+ * A function that does nothing and returns nothing; this is returned from the [[create]] function if no [callable]{@link isCallable} delegates are passed in.
+ * @hidden
+ */
+const noop = (): void => { };
 
 /**
  * Implementation of a multicast delegate to be used in conjunction with the [[create]] function.
@@ -15,30 +21,36 @@ export interface Delegate {
 	/**
 	 * The a prototype for any callable function.
 	 * @param args An arbitory number of parameters to pass to the function.
-	 * @return An optional return from the function.
+	 * @return An optional return from the function. If the delegate is a multicast delegate (one created from multiple other delegates) this will be in the form of an array.
 	 */
 	(...args: any[]): any;
 }
 
-/***
- * A delegate that does nothing; this is always returned from create if no delegates are passed in. This can be used for comparison purposes.
- * @param args An arbitory number of parameters to pass to the function.
+/**
+ * Tests a [delegate]{@link Delegate} to see if is contains callable behavior. 
+ * @param delegate The [delegate]{@link Delegate} to test.
+ * @return Returns true if the [delegate]{@link Delegate} contains callable behavior.
  */
-export const noop: Delegate = (...args: any[]): any => { };
+export function isCallable(delegate: Delegate): boolean {
+	return delegate !== noop && delegate !== undefined && delegate !== null;
+}
 
 /**
- * Creates a delegate for one or more functions that can be called as one.
- * @param delegates The set of functions to aggregate into a single delegate.
- * @return Returns a delegate that when called calls the other functions provided.
+ * Creates a [delegate]{@link Delegate} for one or more [delegates]{@link Delegate} (functions) that can be called as one.
+ * @param delegates The set of [delegates]{@link Delegate} (functions) to aggregate into a single [delegate]{@link Delegate}.
+ * @return Returns a [delegate]{@link Delegate} that when called calls the other [delegates]{@link Delegate} provided.
  */
 export function create(...delegates: Delegate[]): Delegate {
-	const callable = delegates.filter(f => f !== noop && f !== undefined && f !== null);
+	// filter non-callable entries from the passed parameters.
+	const callable = delegates.filter(isCallable);
 
 	if (callable.length !== 0) {
+		// create a new delegate that calls all the passed delegates and returns their results as an array.
 		const delegate = (...args: any[]) => callable.map(f => f(...args));
 
 		return delegate;
 	} else {
+		// as there is nothing to call, return the noop.
 		return noop;
 	}
 }
