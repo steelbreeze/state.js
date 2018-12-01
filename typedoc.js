f235f1a981ec0e215ef691458f5cf67553be467f
Mesmo <mesmo@steelbreeze.net>
2018-12-01 20:06:40 +0000

    Add typedoc params

---
+++ typedoc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	out: '../state/docs/api/v5',
+	readme: 'none',
+	mode: 'file',
+	module: 'common',
+	includeDeclatations: false,
+	excludeExternals: false,
+	excludeNotExported: true,
+	excludePrivate: true,
+	stripInternal: true
+};
